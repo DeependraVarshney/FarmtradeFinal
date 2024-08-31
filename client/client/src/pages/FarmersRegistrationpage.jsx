ce11d80a7870167750956d3cbe44d50a12fd3de1
SagarMaddi <sagarmaddi1402@gmail.com>
2024-08-31 16:15:21 +0530

    hgfahf

---
+++ client/client/src/pages/FarmersRegistrationpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FarmerRegistrationForm from '../component/FarmersRegistration.jsx/src/FarmerRegistrationForm'
+const FarmersRegistrationpage = () => {
+  return (
+    <div>
+      <FarmerRegistrationForm />
+    </div>
+  )
+}
+
+export default FarmersRegistrationpage
